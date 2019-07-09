3c8dbb0d2bf159ccadbea1aa6ebc04f3ab971837
YoHuan <b00502053@ntu.edu.tw>
2019-07-10 01:28:35 +0800

    add more expect tests

--- src/basic/expect.test.js
+++ src/basic/expect.test.js
@@ -1,3 +1,23 @@
+describe('equality', () => {
+  const can1 = {
+    flavor: 'grapefruit',
+    ounces: 12,
+  };
+  const can2 = {
+    flavor: 'grapefruit',
+    ounces: 12,
+  };
+
+  describe('the La Croix cans on my desk', () => {
+    test('have all the same properties', () => {
+      expect(can1).toEqual(can2);
+    });
+    test('are not the exact same can', () => {
+      expect(can1).not.toBe(can2);
+    });
+  });
+});
+
 describe('number', () => {
   test('two plus two', () => {
     const value = 2 + 2;
