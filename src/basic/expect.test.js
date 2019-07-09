eef619ee4e15849875bef18e79f6ad76991821e6
YoHuan <b00502053@ntu.edu.tw>
2019-07-10 01:23:45 +0800

    refactor testing iterable

--- src/basic/expect.test.js
+++ src/basic/expect.test.js
@@ -27,3 +27,18 @@ describe('string', () => {
     expect('Christoph').toMatch(/stop/);
   });
 });
+
+describe('iterable', () => {
+  const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+  ];
+
+  test('the shopping list has beer on it', () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer');
+  });
+});
