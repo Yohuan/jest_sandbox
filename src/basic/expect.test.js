748af8063b24a1417c3d2a9e8aff7408331dc009
YoHuan <b00502053@ntu.edu.tw>
2019-07-10 01:24:56 +0800

    refactor testing truthiness

--- src/basic/expect.test.js
+++ src/basic/expect.test.js
@@ -42,3 +42,24 @@ describe('iterable', () => {
     expect(new Set(shoppingList)).toContain('beer');
   });
 });
+
+describe('truthiness', () => {
+  test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+
+  });
+
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).toBeFalsy();
+    expect(z).not.toBeTruthy();
+  });
+});
