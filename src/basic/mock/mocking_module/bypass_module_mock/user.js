0873a54b80aaec1f7bec2e6f7caaaeb45f1c726b
YoHuan <b00502053@ntu.edu.tw>
2019-07-10 23:42:10 +0800

    bypass mock

---
+++ src/basic/mock/mocking_module/bypass_module_mock/user.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+
+const createUser = async () => {
+  const response = await fetch('http://website.com/users', { method: 'POST' });
+  const userId = await response.text();
+  return userId;
+};
+
+export {
+  createUser,
+};
---
+++ src/basic/mock/mocking_module/bypass_module_mock/user.test.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+import { createUser } from './user';
+
+jest.mock('node-fetch');
+const { Response } = jest.requireActual('node-fetch'); // need the real implementation
+
+test('createUser calls fetch with the right args and returns the user id', async () => {
+  fetch.mockReturnValue(Promise.resolve(new Response('4')));
+
+  const userId = await createUser();
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith('http://website.com/users', {
+    method: 'POST',
+  });
+  expect(userId).toBe('4');
+});
