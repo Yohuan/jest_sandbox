ebd56b72e0316e4ead9ca7ff80c8c2073aca48df
YoHuan <b00502053@ntu.edu.tw>
2019-07-20 15:09:24 +0800

    refactor mock class

--- src/basic/mock/mock_class/sound_player_consumer.test.js
+++ src/basic/mock/mock_class/sound_player_consumer.test.js
@@ -8,13 +8,13 @@ beforeEach(() => {
   SoundPlayer.mockClear();
 });
 
-it('We can check if the consumer called the class constructor', () => {
+test('We can check if the consumer called the class constructor', () => {
   // eslint-disable-next-line no-unused-vars
   const soundPlayerConsumer = new SoundPlayerConsumer();
   expect(SoundPlayer).toHaveBeenCalledTimes(1);
 });
 
-it('We can check if the consumer called a method on the class instance', () => {
+test('We can check if the consumer called a method on the class instance', () => {
   // Show that mockClear() is working:
   expect(SoundPlayer).not.toHaveBeenCalled();
 
@@ -27,8 +27,7 @@ it('We can check if the consumer called a method on the class instance', () => {
   // mock.instances is available with automatic mocks:
   const mockedSoundPlayerInstance = SoundPlayer.mock.instances[0];
   const mockedPlaySoundFile = mockedSoundPlayerInstance.playSoundFile;
-  expect(mockedPlaySoundFile.mock.calls[0][0]).toEqual(coolSoundFileName);
-  // Equivalent to above check:
-  expect(mockedPlaySoundFile).toHaveBeenCalledWith(coolSoundFileName);
+
   expect(mockedPlaySoundFile).toHaveBeenCalledTimes(1);
+  expect(mockedPlaySoundFile).toHaveBeenCalledWith(coolSoundFileName);
 });
