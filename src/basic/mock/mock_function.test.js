f04c28827f6f6d0da99707da54a212ed0d5183f9
YoHuan <b00502053@ntu.edu.tw>
2019-07-20 14:13:42 +0800

    refactor mock function

--- src/basic/mock/mock_function.test.js
+++ src/basic/mock/mock_function.test.js
@@ -1,68 +1,43 @@
 // Inject a callback function
 // Avoid the need for complicated stubs when testing
 function forEach(items, callback) {
-  for (let index = 0; index < items.length; index++) {
-    callback(items[index]);
-  }
+  items.forEach((item) => {
+    callback(item);
+  });
 }
 
-test('mock function', () => {
-  const mockCallback = jest.fn(x => 42 + x);
-  forEach([0, 1], mockCallback);
+test('mock implementation', () => {
+  const mockCallback = jest.fn(x => x * 2);
+  forEach([10, 50], mockCallback);
 
   // The mock function is called twice
   expect(mockCallback.mock.calls.length).toBe(2);
+  expect(mockCallback).toHaveBeenCalled();
+  expect(mockCallback).toHaveBeenCalledTimes(2);
 
   // The first argument of the first call to the function was 0
-  expect(mockCallback.mock.calls[0][0]).toBe(0);
+  expect(mockCallback.mock.calls[0][0]).toBe(10);
+  expect(mockCallback).toHaveBeenNthCalledWith(1, 10);
 
   // The first argument of the second call to the function was 1
-  expect(mockCallback.mock.calls[1][0]).toBe(1);
+  expect(mockCallback.mock.calls[1][0]).toBe(50);
+  expect(mockCallback).toHaveBeenNthCalledWith(2, 50);
 
   // The return value of the first call to the function was 42
-  expect(mockCallback.mock.results[0].value).toBe(42);
+  expect(mockCallback.mock.results[0].value).toBe(20);
+  expect(mockCallback).toHaveNthReturnedWith(1, 20);
 
-  expect(mockCallback.mock.results[1].value).toBe(43);
+  expect(mockCallback.mock.results[1].value).toBe(100);
+  expect(mockCallback).toHaveNthReturnedWith(2, 100);
 });
 
-describe('mock returned value', () => {
-  test('mock returned value', () => {
-    const myMock = jest.fn();
-    myMock
-      .mockReturnValueOnce(10)
-      .mockReturnValueOnce('x')
-      .mockReturnValue(true);
-    expect(myMock()).toBe(10);
-    expect(myMock()).toBe('x');
-    expect(myMock()).toBe(true);
-    expect(myMock()).toBe(true);
-  });
-});
-
-function drinkAll(callback, flavors) {
-  flavors.forEach((flavor) => {
-    if (flavor !== 'octopus') {
-      callback(flavor);
-    }
-  });
-}
-
-describe('drinkAll', () => {
-  test('drinks something lemon-flavoured', () => {
-    const drink = jest.fn();
-    drinkAll(drink, ['lemon']);
-    expect(drink).toHaveBeenCalled();
-  });
-
-  test('does not drink something octopus-flavoured', () => {
-    const drink = jest.fn();
-    drinkAll(drink, ['octopus']);
-    expect(drink).not.toHaveBeenCalled();
-  });
-
-  test('drinkEach drinks each drink', () => {
-    const drink = jest.fn();
-    drinkAll(drink, ['lemon', 'apple']);
-    expect(drink).toHaveBeenCalledTimes(2);
-  });
+test('mock returned value', () => {
+  const myMock = jest.fn();
+  myMock.mockReturnValueOnce(10)
+    .mockReturnValueOnce('x')
+    .mockReturnValue(true);
+  expect(myMock()).toBe(10);
+  expect(myMock()).toBe('x');
+  expect(myMock()).toBe(true);
+  expect(myMock()).toBe(true);
 });
