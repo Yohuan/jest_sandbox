bdb25bc41d40640550905efbd6e453d727d1357a
YoHuan <b00502053@ntu.edu.tw>
2019-07-21 02:12:09 +0800

    refactor asynchronous

--- src/basic/asynchronous/callback.test.js
+++ src/basic/asynchronous/callback.test.js
@@ -3,10 +3,11 @@ const fetchData = (callback) => {
   callback(data);
 };
 
+// Give a "done" function as input
 test('the data is peanut butter', (done) => {
   function callback(data) {
     expect(data).toBe('peanut butter');
-    // Jest will wait until the done callback is called before finishing the test.
+    // Jest will wait until the done() is called before finishing the test.
     // If done() is never called, the test will fail.
     done();
   }
